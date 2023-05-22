189e4edb24dfbe7aa85c727a1a605729f50fe9ff
Anatoly Fedyanin <anfemail@gmail.com>
2023-05-22 22:16:03 +0300

    fix

--- src/Vertr.OrderMatching.Application/Commands/Sell/SellCommandHandler.cs
+++ src/Vertr.OrderMatching.Application/Commands/Sell/SellCommandHandler.cs
@@ -1,15 +1,15 @@
 using MediatR;
 
-namespace Vertr.OrderMatching.Application.Commands.Buy
+namespace Vertr.OrderMatching.Application.Commands.Sell
 {
-    public sealed class BuyCommandHandler : IRequestHandler<BuyLimitCommand>, IRequestHandler<BuyMarketCommand>
+    public sealed class SellCommandHandler : IRequestHandler<SellLimitCommand>, IRequestHandler<SellMarketCommand>
     {
-        public Task Handle(BuyLimitCommand request, CancellationToken cancellationToken)
+        public Task Handle(SellLimitCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
 
-        public Task Handle(BuyMarketCommand request, CancellationToken cancellationToken)
+        public Task Handle(SellMarketCommand request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
