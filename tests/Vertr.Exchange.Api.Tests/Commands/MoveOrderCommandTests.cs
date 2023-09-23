46a25fc29b2de20bbce337d59d5a7fc3255a9642
Anatoly Fedyanin <anfemail@gmail.com>
2023-09-23 11:06:34 +0300

    kjh

---
+++ tests/Vertr.Exchange.Api.Tests/Commands/MoveOrderCommandTests.cs
@@ -0,0 +1,29 @@
+using Vertr.Exchange.Api.Commands;
+using Vertr.Exchange.Common.Enums;
+
+namespace Vertr.Exchange.Api.Tests.Commands;
+
+[TestFixture(Category = "Unit")]
+public class MoveOrderCommandTests : CommandTestBase
+{
+    [Test]
+    public async Task CanMoveOrder()
+    {
+        var uid = 100L;
+        await AddUser(uid);
+
+        var symbol = 2;
+        await AddSymbol(symbol);
+
+        var res = await PlaceGTCOrder(OrderAction.BID, uid, symbol, 23.45m, 34);
+        var orderId = res.OrderId;
+
+        var move = new MoveOrderCommand(orderId, DateTime.UtcNow, uid, 23.56m, symbol);
+        res = await Api.SendAsync(move);
+
+        Assert.That(res.ResultCode, Is.EqualTo(CommandResultCode.SUCCESS));
+
+        // TODO: Check order book
+        // TODO: Handle case with matching order
+    }
+}
