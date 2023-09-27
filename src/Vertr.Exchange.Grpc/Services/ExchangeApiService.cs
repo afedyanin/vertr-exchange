15ad4ae425292cf0707bed81b5c5cee866ed4110
Anatoly Fedyanin <anfemail@gmail.com>
2023-09-27 19:32:19 +0300

    asd

--- src/Vertr.Exchange.Grpc/Services/ExchangeApiService.cs
+++ src/Vertr.Exchange.Grpc/Services/ExchangeApiService.cs
@@ -14,7 +14,7 @@ public class ExchangeApiService : ExchangeApi.ExchangeApiBase
 
     public override Task<ApiCommandResult> Nop(ApiCommandNoParams request, ServerCallContext context)
     {
-        _logger.LogDebug("Receiveing NOP command");
+        _logger.LogDebug("NOP command received.");
 
         var res = new ApiCommandResult()
         {
@@ -24,5 +24,40 @@ public class ExchangeApiService : ExchangeApi.ExchangeApiBase
         };
 
         return Task.FromResult(res);
+    }
+
+    public override Task<ApiCommandResult> GetOrderBook(OrderBookRequest request, ServerCallContext context)
+    {
+        _logger.LogDebug("GetOrderBook command received.");
+
+        var res = new ApiCommandResult()
+        {
+            CommandResultCode = CommandResultCode.Success,
+            OrderId = 1000L,
+            Timestamp = DateTime.UtcNow.ToTimestamp(),
+            MarketData = CreateMarketData(),
+        };
+
+        return Task.FromResult(res);
+    }
+
+    private static L2MarketData CreateMarketData()
+    {
+        var res = new L2MarketData
+        {
+            AskSize = 1,
+            BidSize = 1,
+            Timestamp = DateTime.UtcNow.ToTimestamp(),
+            ReferenceSeq = 1,
+        };
+
+        res.AskVolumes.AddRange(new long[] { 1, 2, 3, 4 });
+        res.BidVolumes.AddRange(new long[] { 1, 2, 3, 4 });
+        res.AskOrders.AddRange(new long[] { 10, 20, 30, 40 });
+        res.BidOrders.AddRange(new long[] { 11, 21, 31, 41 });
+        res.AskPrices.AddRange(new DecimalValue[] { 1.34m, 2.45m, 3.67m, 4.85m });
+        res.BidPrices.AddRange(new DecimalValue[] { 1.34m, 2.45m, 3.67m, 4.85m });
+
+        return res;
     }
 }
--- tests/Vertr.Exchange.Grpc.Tests/ExchangeApiServiceTests.cs
+++ tests/Vertr.Exchange.Grpc.Tests/ExchangeApiServiceTests.cs
@@ -6,7 +6,7 @@ namespace Vertr.Exchange.Grpc.Tests;
 public class ExchangeApiServiceTests
 {
     [Test]
-    public async Task CanCallNopCommand()
+    public async Task NopCommand()
     {
         using var channel = GrpcChannel.ForAddress("http://localhost:5294");
 
@@ -19,6 +19,31 @@ public class ExchangeApiServiceTests
             Assert.That(result.CommandResultCode, Is.EqualTo(CommandResultCode.Success));
             Assert.That(result.OrderId, Is.GreaterThan(0L));
             Assert.That(result.Timestamp.ToDateTime(), Is.GreaterThan(DateTime.MinValue));
+            Assert.That(result.MarketData, Is.Null);
         });
+    }
+
+    [Test]
+    public async Task GetOrderBookCommand()
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5294");
+
+        var client = new ExchangeApi.ExchangeApiClient(channel);
+        var result = await client.GetOrderBookAsync(new OrderBookRequest
+        {
+            Symbol = 1,
+            Size = 100
+        });
+
+        Assert.That(result, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.CommandResultCode, Is.EqualTo(CommandResultCode.Success));
+            Assert.That(result.MarketData, Is.Not.Null);
+        });
+
+        var prices = result.MarketData.AskPrices.Select(p => p.ToDecimal());
+        var str = string.Join(";", prices);
+        Console.WriteLine(str);
     }
 }
