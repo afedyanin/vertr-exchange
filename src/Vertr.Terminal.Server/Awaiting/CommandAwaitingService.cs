e547e37d22f0f6df3538068de73684c55cc15268
Anatoly Fedyanin <anfemail@gmail.com>
2023-12-11 21:58:34 +0300

    fix lock

--- src/Vertr.Terminal.Server/Awaiting/CommandAwaitingService.cs
+++ src/Vertr.Terminal.Server/Awaiting/CommandAwaitingService.cs
@@ -7,47 +7,57 @@ internal sealed class CommandAwaitingService(ILogger<CommandAwaitingService> log
     private readonly ConcurrentDictionary<long, TaskCompletionSource<CommandResponse>> _commands =
         new ConcurrentDictionary<long, TaskCompletionSource<CommandResponse>>();
 
-    private readonly ConcurrentDictionary<long, CommandResponse> _missedCommands =
-        new ConcurrentDictionary<long, CommandResponse>();
-
     private readonly ILogger<CommandAwaitingService> _logger = logger;
+
+    private static readonly object _lock = new object();
 
     public Task<CommandResponse> Register(long commandId, CancellationToken cancellationToken = default)
     {
-        var tcs = new TaskCompletionSource<CommandResponse>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-        // Check if it already done.
-        if (_missedCommands.TryRemove(commandId, out var res))
+        lock (_lock)
         {
-            tcs.SetResult(res);
+            // Check if it is already done.
+            if (_commands.TryRemove(commandId, out var tcs))
+            {
+                _logger.LogInformation("Command Id={commandId} already completed.", commandId);
+                return tcs.Task;
+            }
+
+            tcs = new TaskCompletionSource<CommandResponse>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            if (!_commands.TryAdd(commandId, tcs))
+            {
+                throw new InvalidOperationException($"Command Id={commandId} is already registered.");
+            }
+
+            _logger.LogInformation("Command Id={commandId} is registered.", commandId);
+
+            cancellationToken.Register(UnregisterWait, new WaitContext(commandId, cancellationToken));
+
             return tcs.Task;
         }
-
-        if (!_commands.TryAdd(commandId, tcs))
-        {
-            throw new InvalidOperationException($"Command with Id={commandId} is already registered.");
-        }
-
-        _logger.LogDebug("Command with Id={commandId} is registered.", commandId);
-
-        cancellationToken.Register(UnregisterWait, new WaitContext(commandId, cancellationToken));
-
-        return tcs.Task;
     }
 
     public void Complete(CommandResponse response)
     {
-        var commandId = response.CommandResult.OrderId;
+        lock (_lock)
+        {
+            var commandId = response.CommandResult.OrderId;
 
-        if (_commands.TryRemove(commandId, out var tcs))
-        {
-            _logger.LogDebug("Command with Id={commandId} is completed.", commandId);
+            if (_commands.TryRemove(commandId, out var tcs))
+            {
+                _logger.LogInformation("Command Id={commandId} is completed.", commandId);
+                tcs.SetResult(response);
+                return;
+            }
+
+            _logger.LogWarning("Command Id={commandId} completed before awating.", commandId);
+            tcs = new TaskCompletionSource<CommandResponse>(TaskCreationOptions.RunContinuationsAsynchronously);
             tcs.SetResult(response);
-        }
-        else
-        {
-            _logger.LogWarning("Command with Id={commandId} is not awaited.", commandId);
-            _missedCommands.TryAdd(response.CommandResult.OrderId, response);
+
+            if (!_commands.TryAdd(response.CommandResult.OrderId, tcs))
+            {
+                throw new InvalidOperationException($"Command Id={commandId} is already registered.");
+            }
         }
     }
 
--- tests/Vertr.Terminal.Server.Tests.ConsoleApp/Program.cs
+++ tests/Vertr.Terminal.Server.Tests.ConsoleApp/Program.cs
@@ -8,6 +8,62 @@ internal sealed class Program
 {
     public static async Task Main()
     {
+        await Test001();
+    }
+
+    public static async Task Test003()
+    {
+        var logger = LoggerStub.CreateConsoleLogger<CommandAwaitingService>();
+        var service = new CommandAwaitingService(logger);
+
+        Console.WriteLine("T1 starting...");
+
+        var t1 = Task.Run(async () =>
+        {
+            var apiResult = new ApiCommandResult
+            {
+                OrderId = 100L,
+            };
+
+            var resp = new CommandResponse(apiResult);
+            service.Complete(resp);
+            await Task.Delay(10);
+        });
+
+        Console.WriteLine("T2 starting...");
+        var t2 = Task.Run(async () =>
+        {
+            await Task.Delay(10);
+            var response = await service.Register(100L);
+        });
+
+        Console.WriteLine("Awaiting T1 & T2");
+        await Task.WhenAll(t1, t2);
+
+        Console.WriteLine("Completed.");
+    }
+
+    public static async Task Test002()
+    {
+        var logger = LoggerStub.CreateConsoleLogger<CommandAwaitingService>();
+        var service = new CommandAwaitingService(logger);
+
+        var apiResult = new ApiCommandResult
+        {
+            OrderId = 100L,
+        };
+
+        var resp = new CommandResponse(apiResult);
+        service.Complete(resp);
+
+        await Task.Delay(200);
+        var response = await service.Register(100L);
+
+        Console.WriteLine($"Completed. OrderId={response.CommandResult.OrderId}");
+    }
+
+    public static async Task Test001()
+    {
         var logger = LoggerStub.CreateConsoleLogger<CommandAwaitingService>();
         var service = new CommandAwaitingService(logger);
 
@@ -31,7 +87,7 @@ internal sealed class Program
 
     private static async Task RegisterWait(CommandAwaitingService service)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 100; i++)
         {
             Console.WriteLine($"Register #{i}");
             var response = await service.Register(i);
@@ -42,7 +98,7 @@ internal sealed class Program
 
     private static async Task SetCompleted(CommandAwaitingService service)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 100; i++)
         {
             var apiResult = new ApiCommandResult
             {
