c6e735aeb364bf9aae10cd1d7bdcaf35dddbd169
Anatoly Fedyanin <anfemail@gmail.com>
2023-12-31 12:03:35 +0300

    eq

--- tests/Vertr.Exchange.Accounts.Tests/PositionTests.cs
+++ tests/Vertr.Exchange.Accounts.Tests/PositionTests.cs
@@ -97,14 +97,29 @@ public class PositionTests
     }
 
     [Test]
+    public void LongCanUpdateWithAskClosePosition()
+    {
+        var pos = new Position(1L, 2);
+        pos.Update(OrderAction.BID, 100, 10m);
+        Console.WriteLine($"Bid 100x10$ (1000) => {pos}");
+        pos.Update(OrderAction.ASK, 90, 100m);
+        Console.WriteLine($"Ask 90x100$ (-9000) => {pos}");
+        pos.Update(OrderAction.ASK, 5, 100m);
+        Console.WriteLine($"Ask 5x100$ (-500) => {pos}");
+        pos.Update(OrderAction.ASK, 5, 100m);
+        Console.WriteLine($"Ask 5x100$ (-500) => {pos}");
+    }
+
+    [Test]
     public void LongCanUpdateWithAskOverlap()
     {
         var pos = new Position(1L, 2);
         pos.Update(OrderAction.BID, 100, 10m);
+        Console.WriteLine($"Bid 100x10$ (1000) => {pos}");
         pos.Update(OrderAction.ASK, 90, 100m);
-        Console.WriteLine(pos);
+        Console.WriteLine($"Ask 90x100$ (-9000) => {pos}");
         pos.Update(OrderAction.ASK, 5, 100m);
-        Console.WriteLine(pos);
+        Console.WriteLine($"Ask 5x100$ (-500) => {pos}");
         pos.Update(OrderAction.ASK, 15, 20m);
         Console.WriteLine(pos);
     }
